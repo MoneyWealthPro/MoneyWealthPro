880902e417917ec583a096624669d376948d67d4
femithz <ajayiadeyinka6991@gmail.com>
2022-06-24 21:52:29 +0100

    update

--- src/app/core/auth/logout-modal/logout-modal.component.ts
+++ src/app/core/auth/logout-modal/logout-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { NotifierService } from 'angular-notifier';
 
 @Component({
   selector: 'app-logout-modal',
@@ -9,12 +10,15 @@ import { Router } from '@angular/router';
 })
 export class LogoutModalComponent implements OnInit {
 
-  constructor(private dialog: MatDialog, private router: Router) { }
+  constructor(private dialog: MatDialog, 
+    private router: Router,
+    private notifierService: NotifierService) { }
 
   ngOnInit(): void {
   }
   logout () {
     localStorage.clear();
+    this.notifierService.notify('success', `Logout successfully`);
     this.router.navigate(['/home']);
     this.dialog.closeAll();
   }
