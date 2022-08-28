b55f2d8f7e0ac1ef9bfd4a9c48a9029a77303de5
femithz <ajayiadeyinka6991@gmail.com>
2022-08-28 09:09:59 +0100

    corrections added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 export const environment = {
   production: false,
-  APIURL: 'http://localhost:3000/'
+  APIURL: 'https://investment-backend-api.herokuapp.com/'
+  // http://localhost:3000/'
   // 'https://investment-backend-api.herokuapp.com/'
 };
 
